752b334e07a6702e515a6a335e24cae461d18b4f
[Dorian Hawkins] <dgh49@case.edu>
2024-11-14 17:28:54 -0500

    Initial Commit

---
+++ Map Prototype/Map_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_PrototypeApp.swift
+//  Map Prototype
+//
+//  Created by Dorian Hawkins on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Map_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Map PrototypeTests/Map_PrototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Map_PrototypeTests.swift
+//  Map PrototypeTests
+//
+//  Created by Dorian Hawkins on 11/14/24.
+//
+
+import Testing
+@testable import Map_Prototype
+
+struct Map_PrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
